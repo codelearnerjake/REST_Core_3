5971b6696ad5c6a3da7124362559edcbd4484494
Jake Borchers <jborchers@bergenlogistics.com>
2021-04-22 12:18:58 -0400

    Adding GetAuthors() method

---
+++ REST_Core_3/Controllers/AuthorsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CourseLibrary.API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_Core_3.Controllers
+{
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository; 
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+        [HttpGet("api/authors")]
+
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
